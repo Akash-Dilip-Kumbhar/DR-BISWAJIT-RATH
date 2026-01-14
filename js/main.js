52e20e0efee4a6b7aecd97dc17e391c6c0fcf087
Akash Laptop <Akashkumbhar89@gmail.com>
2026-01-14 12:49:03 +0530

    slider js add

--- js/main.js
+++ js/main.js
@@ -137,3 +137,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide('#splide', {
+    type: 'loop',
+    perPage: 2,
+    focus    : 'center',    
+    autoplay: true,
+    interval: 2500,
+    flickMaxPages: 2,
+    updateOnMove: true,
+    pagination: false,
+    throttle: 300,
+}).mount();
+});
